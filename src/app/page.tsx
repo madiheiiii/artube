e86411cedc895ea867f56f4b6dd63220393dff9d
Maddie <mirtorabi1234@gmail.com>
2025-09-14 11:55:34 -0400

    this commit adds shadcn ui with changes in page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return(
     <div>
-      <p>
-        Hello World!
-      </p>
+      <Button variant="destructive">
+        click me!
+      </Button>
     </div>
   )
 }
